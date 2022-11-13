6590ba94f54c25cfe6c92fdaf27f61a998892644
mikekondo <foad5969@gmail.com>
2022-11-14 07:16:06 +0900

    fix: viewの調整

--- SwiftUI-LandmarksApp/ContentView.swift
+++ SwiftUI-LandmarksApp/ContentView.swift
@@ -10,6 +10,14 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
+            MapView()
+                .ignoresSafeArea(edges: .top) // mapを画面上端に
+                .frame(height: 300)
+
+            CircleImage()
+                .offset(y: -130) // 座標指定
+                .padding(.bottom,-130)
+
             VStack(alignment: .leading) {
                 Text("Turtle Rock")
                     .font(.title)
@@ -20,12 +28,18 @@ struct ContentView: View {
                     Text("California")
                         .font(.subheadline)
                 }
+                .font(.subheadline)
+                .foregroundColor(.secondary) // 住所をグレー色
+
+                Divider() // 仕切り
+
+                Text("About Turtle Rock")
+                    .font(.title2)
+                Text("Descriptive text goes here.")
             }
             .padding()
-
-
+            Spacer()
         }
-        .padding()
     }
 }
 
