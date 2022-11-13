a342a904622ebbfdabdd510f511bcc549b980768
mikekondo <foad5969@gmail.com>
2022-11-13 11:22:05 +0900

    image追記

---
+++ SwiftUI-LandmarksApp/CircleImage.swift
@@ -0,0 +1,25 @@
+//
+//  CircleImage.swift
+//  SwiftUI-LandmarksApp
+//
+//  Created by 近藤米功 on 2022/11/13.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay {
+                Circle().stroke(.white,lineWidth: 4)
+            }
+            .shadow(radius: 7)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
