a8f37d23a25a83116f68e843988cd702612a7efd
mikekondo <foad5969@gmail.com>
2022-11-16 07:29:02 +0900

    add: Detailの追記

--- SwiftUI-LandmarksApp/Views/LandmarkList.swift
+++ SwiftUI-LandmarksApp/Views/LandmarkList.swift
@@ -9,9 +9,17 @@ import SwiftUI
 
 struct LandmarkList: View {
     var body: some View {
-        // MARK: データのコレクションと、コレクションの各要素のビューを提供するクロージャを渡す(LandmarkはIdentifiableを準拠させる）
-        List(landmarks) { landmark in
-            LandmarkRow(landmark: landmark)
+        NavigationView {
+            // MARK: データのコレクションと、コレクションの各要素のビューを提供するクロージャを渡す(LandmarkはIdentifiableを準拠させる）
+            List(landmarks) { landmark in
+                // LandmarkRow(landmark: landmark)
+                NavigationLink {
+                    LandmarkDetail()
+                } label: {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationTitle("Landmarks")
         }
     }
 }
